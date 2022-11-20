f1d30f7ea8e758b4a047b97b79f0e53b3f91dba2
Pearlmulinde <pearlmulind@yahoo.com>
2022-11-20 15:34:39 +0100

    Create opdrachten15.php

---
+++ opdrachten15.php
@@ -0,0 +1,45 @@
+<?php
+$weeklyTemps = [
+    "Maandag" => [ 
+    	"Groningen" => 25,
+        	"Assen" => 12,
+       	 "Emmen" => 19
+    ],
+     "Dinsdag" => [ 
+    	"Groningen" => 26,
+        "Assen" => 28,
+        "Emmen" => 19
+    ],
+     "Woensdag" => [ 
+    	"Groningen" => 16,
+        "Assen" => 30,
+        "Emmen" => 35
+    ]
+];
+$array=[];
+    // for ($i= 0; $i<3; $i++){
+
+$temp = array();
+foreach ($weeklyTemps as $item) {
+    foreach ($item as $key=>$value)
+     {
+        $temp[$key] = max(isset($temp[$key]) ? $temp[$key] : $value,$value);
+        $var= $temp[$key];
+         
+    }
+    // array_push($array,$var);
+}
+    
+//  array_push($array,$key);
+ echo '<pre>';
+ print_r($temp);
+ echo '<pre>';
+
+//print_r("The highest temperature in " ."$key is $value");
+//echo  $temp[$key];
+?>
+
+
+
+
+
